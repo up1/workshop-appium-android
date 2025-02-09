a0fba1c4ed056dc3c0ca1c5f32c40ee2fb7028b2
up1 <somkiat.p@gmail.com>
2025-02-09 23:05:17 +0700

    Add accessibility id

--- app/src/main/java/com/demo/mylogin/ui/screens/unauthenticated/login/LoginInputs.kt
+++ app/src/main/java/com/demo/mylogin/ui/screens/unauthenticated/login/LoginInputs.kt
@@ -9,8 +9,11 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.layoutId
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.contentDescription
+import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.style.TextAlign
 import com.demo.mylogin.R
@@ -38,7 +41,9 @@ fun LoginInputs(
                 .fillMaxWidth()
                 .padding(top = AppTheme.dimens.paddingLarge)
                 .testTag("testTagLoginName")
-//                .testTag("android:id/`testTagLoginName`")
+                .semantics {
+                    contentDescription = "login_name"
+                }
             ,
             value = loginState.emailOrMobile,
             onValueChange = onEmailOrMobileChange,
@@ -52,7 +57,8 @@ fun LoginInputs(
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(top = AppTheme.dimens.paddingLarge)
-                .testTag("testTagLoginPassword"),
+                .testTag("testTagLoginPassword")
+                .layoutId("password_id"),
             value = loginState.password,
             onValueChange = onPasswordChange,
             label = stringResource(id = R.string.login_password_label),
